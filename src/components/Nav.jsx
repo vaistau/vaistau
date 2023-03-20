bd8e27a6075f030526a2567ac8179ac847618bda
Vaiva <vaistau@proton.me>
2023-03-20 15:11:41 -0500

    idk

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const Nav = () => {
   return (
-    <nav className="width-wrapper py-2">
-      <div className="flex items-end gap-1">
-        <h1 className="leading-none">[vaiva:~]$ ls links</h1>
-        <span className="w-5 h-2 bg-black inline-block gap-1 animate-cursor-blink" />
-      </div>
+    <nav className="width-wrapper py-8">
+      <Link to="/" className="flex items-end gap-1">
+        <h1 className="leading-none text-5xl">[vaiva:~]$ ls links</h1>
+        <span className="w-10 h-3 bg-black inline-block gap-1 animate-cursor-blink" />
+      </Link>
     </nav>
   );
 };
